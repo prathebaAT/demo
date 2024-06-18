af5f620537f948aef73fa16636402887e181aecb
prathebaAT <pratheba63@gmail.com>
2024-06-18 11:23:33 +0530

    created controller

---
+++ src/main/java/com/example/demo/controller/WebController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class WebController {
+
+    @GetMapping("/")
+    public String Home() {
+        return "index";
+    }
+    
+
+    
+}
